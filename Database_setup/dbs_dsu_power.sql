e14f975a6b437f2401a35e635dddfbf10b0d2f51
gurkaran27 <a00258749@student.ait.ie>
2019-08-01 02:55:08 +0100

    SQL Dumps

---
+++ Database_setup/dbs_dsu_power.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `dbs` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dbs`;
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbs
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dsu_power`
+--
+
+DROP TABLE IF EXISTS `dsu_power`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `dsu_power` (
+  `dsu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `total_power` decimal(13,4) DEFAULT NULL,
+  `time_aggregated` datetime DEFAULT NULL,
+  PRIMARY KEY (`dsu_id`),
+  CONSTRAINT `dsus_id` FOREIGN KEY (`dsu_id`) REFERENCES `dsus` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dsu_power`
+--
+
+LOCK TABLES `dsu_power` WRITE;
+/*!40000 ALTER TABLE `dsu_power` DISABLE KEYS */;
+INSERT INTO `dsu_power` VALUES (1,1056.0000,'2019-07-05 01:47:42'),(2,431.0000,'2019-07-05 01:47:42'),(3,563.0000,'2019-07-05 01:47:42'),(4,0.0000,'2019-07-05 01:36:38');
+/*!40000 ALTER TABLE `dsu_power` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-05  1:47:42
